a817132d3bbc0ed757859b595d7be511afef8c4f
Im-Beast <franik.mateusz@gmail.com>
2024-09-03 20:14:05 +0200

    feat: support explicitely specifying dependencies in `computed`/`ComputedSignal`

--- src/computed.ts
+++ src/computed.ts
@@ -1,28 +1,66 @@
 import { $access, $value, BaseSignal } from "./base.ts";
 import { DependantSignal } from "./dependant.ts";
 
+// TODO: LazyComputed
 // TODO: Only watch root dependencies
-// TODO: computed([a, b], (a, b) => ...)
 
-export function computed<T>(computation: () => T): ComputedSignal<T> {
-    return new ComputedSignal(computation);
+export type SignalValues<T extends BaseSignal[]> = {
+    [Key in keyof T]: T[Key] extends BaseSignal<infer Value> ? Value : never;
+};
+
+export function computed<T>(computation: () => T): ComputedSignal<T>;
+export function computed<T, const D extends BaseSignal[]>(
+    dependencies: D,
+    computation: (...args: SignalValues<D>) => T,
+): ComputedSignal<T, D>;
+export function computed<T, const D extends BaseSignal[]>(
+    dependenciesOrComputation?: (() => T) | D,
+    computation?: () => T,
+): ComputedSignal<T, D> {
+    if (typeof dependenciesOrComputation === "function") {
+        const computation = dependenciesOrComputation;
+        return new ComputedSignal(computation);
+    }
+
+    const dependencies = dependenciesOrComputation;
+    return new ComputedSignal(computation!, dependencies);
 }
 
 export function effect(effect: () => void): ComputedSignal<void> {
     return new ComputedSignal(effect);
 }
 
-export class ComputedSignal<T> extends DependantSignal<T> {
-    constructor(computation: () => T) {
-        super(undefined as T, () => {
+export class ComputedSignal<T, D extends BaseSignal[] = []> extends DependantSignal<T> {
+    constructor(computation: (...args: SignalValues<D>) => T, dependencies?: D);
+    constructor(computation: () => T);
+    constructor(computation: (...args: unknown[]) => T, dependencies?: BaseSignal[]) {
+        if (dependencies) {
+            console.log("Has dependencies");
+            super(computation(...dependencies.map((dependency) => dependency[$value]!)), () => {
+                this[$value] = computation(
+                    ...dependencies.map((dependency) => dependency[$value]!),
+                );
+                this.updateDependants();
+            });
+
+            for (const dependency of dependencies) {
+                this[$access](dependency);
+            }
+            return;
+        }
+
+        super(undefined!, () => {
             this[$value] = computation();
             this.updateDependants();
         });
 
-        // TODO: destroy if no signals were associated with this
         BaseSignal.activeSignal = this;
         this[$value] = computation();
         BaseSignal.activeSignal = undefined;
+
+        if (this.dependencies.size === 0) {
+            this[Symbol.dispose]();
+        }
     }
 
     [$access](signal: BaseSignal): void {
