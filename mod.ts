e3298edf45d09c4a2dc1f403aefb13d31747f2a1
Im-Beast <franik.mateusz@gmail.com>
2024-09-05 15:03:26 +0200

    fix: export `lazy_computed.ts`

--- mod.ts
+++ mod.ts
@@ -2,6 +2,7 @@ export { BaseSignal, getValue, type MaybeSignal } from "./src/base.ts";
 export { computed, ComputedSignal, effect } from "./src/computed.ts";
 export { DependantSignal } from "./src/dependant.ts";
 export { Signal, signal } from "./src/signal.ts";
+export { lazyComputed, LazyComputedSignal, lazyEffect } from "./src/lazy_computed.ts";
 
 export { getIntermediate, isObservable } from "./src/observables/shared.ts";
 export { deepObservableArray, type ObservableArray, shallowObservableArray } from "./src/observables/array.ts";
